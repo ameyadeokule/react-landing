d4a6fda3b233263e1202412e1a0bb2dfd6295deb
Ameya Deokule <deokuleameya@gmail.com>
2021-04-03 15:08:06 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useState, useEffect } from 'react'
+
 import './App.css'
+import Dropdown from './components/Dropdown'
+import Navbar from './components/Navbar'
 
 function App() {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggle = () => {
+		setIsOpen(!isOpen)
+	}
+
+	useEffect(() => {
+		const hideMenu = () => {
+			if (window.innerWidth > 768 && isOpen === true) {
+				setIsOpen(false)
+			}
+		}
+		window.addEventListener('resize', hideMenu)
+		return () => {
+			window.removeEventListener('resize', hideMenu)
+		}
+	})
+
 	return (
 		<div className='App'>
-			<h1 className='text-red-700'>hello</h1>
+			<Navbar toggle={toggle} />
+			<Dropdown isOpen={isOpen} toggle={toggle} />
 		</div>
 	)
 }
