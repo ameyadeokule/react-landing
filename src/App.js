b466db4f800f5086627c0183b091d11fc8a17391
Ameya Deokule <deokuleameya@gmail.com>
2021-04-03 14:15:19 +0530

    Redesigning the webpage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import './App.css'
 
 function App() {
-  const [isAnimated, setIsAnimated] = useState(false)
-  
-  useEffect(() => {
-    setIsAnimated(true)
-   
-  }, [isAnimated])
-  return (
-    <div className='min-h-screen w-screen flex justify-center item-center flex-col-reverse lg:flex-row lg:justify-around' style={{background:'linear-gradient(to bottom, #F0F4FD,#A1A3BA)'}}>
-      
-      <div className={`${isAnimated?'scale-y-100':'scale-y-0'} mt-10 text-center lg:text-left transform transition duration-1000 ease-in-out`}>
-        <h1 className='font-black text-5xl lg:text-6xl'>Fresh Healthy<br /> <span className='font-quicksand font-light'>Delicious Salads</span></h1>
-        <p className='text-base text-grey-700'>We make fresh and healthy food </p>
-        <span className='inline-block bg-indigo-500 px-10 py-5 rounded-full shadow-lg uppercase text-lg tracking-wide mt-5'>Explore</span>
-      </div>
-      
-      
-      <img src={process.env.PUBLIC_URL + '/assets/food.svg'} alt='food' className={`${ isAnimated ? 'scale-150' : 'translate-x-full translate-y-full'} w-screen lg:max-w-xl transform transition duration-1000 ease-in-out`}/>
-      
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1 className='text-red-700'>hello</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
