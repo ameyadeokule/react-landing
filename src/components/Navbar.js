7fc4bc92836d2e1f957e0fd2f30c43dc62b7e008
Ameya Deokule <deokuleameya@gmail.com>
2021-04-03 16:35:04 +0530

    Navbar is sticky now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = ({ toggle }) => {
 	return (
 		<nav
-			className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono'
+			className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono sticky top-0'
 			role='navigation'>
 			<Link to='/' className='pl-8'>
 				EGG
